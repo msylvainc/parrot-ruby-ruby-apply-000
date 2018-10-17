783c02043b233f6ebbf9335a6273dea3f4f705ad
Michael Cerasi <michaelcerasi@hotmail.com>
2018-10-17 12:15:59 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,9 @@
-def parrot = {"Squawk!"}
-def parrot1 = {"Pretty bird!"}
-
+def parrot
+  "Squawk!"
+end
 puts parrot "The parrot says #{parrot}."
+
+def parrot1
+  "Pretty bird!"
+end
 puts parrot1 "The parrot says #{parrot1}."
